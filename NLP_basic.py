fde8181aa3c266b650a043d8625d6b0f893b3770
Niklas Lange <98560494+HerrNiklasLange@users.noreply.github.com>
2025-09-12 22:49:22 +0100

    update

--- NLP_basic.py
+++ NLP_basic.py
@@ -7,25 +7,42 @@ nltk.download('all')
 print("here is the dataframe")
 
 # read by default 1st sheet of an excel file
-dataframe1 = pd.read_excel('pres_inuagural_address.xlsx')
-i = 0 
-for i in range(dataframe1.shape[0]):
-    print(i)
-    #dataframe1["speech"][i]
-print(dataframe1["speech"][0])
+df_speech = pd.read_excel('pres_inuagural_address.xlsx')
 
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-# Tokenization
-tokens = word_tokenize(dataframe1["speech"][0])
-print(tokens)
-# Remove stop words
-stop_words = set(stopwords.words('english'))
-filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
-print(filtered_tokens)
-# Lemmatization
-lemmatizer = WordNetLemmatizer()
-lemmatized_tokens = [lemmatizer.lemmatize(word) for word in filtered_tokens]
-print(lemmatized_tokens)
-# POS tagging
+
+def preprocess_text(text):
+    # Tokenization
+    tokens = nltk.word_tokenize(text)
+    
+    # Lowercasing
+    tokens = [token.lower() for token in tokens]
+    
+    # Remove punctuation and non-alphabetic characters
+    tokens = [token for token in tokens if token.isalpha()]
+    
+    # Remove stop words
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stop_words]
+    
+    # Stemming or Lemmatization (using stemming here)
+    stemmer = nltk.PorterStemmer()
+    tokens = [stemmer.stem(token) for token in tokens]
+    
+    return tokens
+
+df_speech['processed_speech'] = df_speech['speech'].apply(preprocess_text)
+print(df_speech.head())
+
+def sum_graph(df):
+    import matplotlib.pyplot as plt
+    from wordcloud import WordCloud
+
+    all_words = ' '.join([' '.join(map(str, tokens)) for tokens in df['processed_speech']])
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(all_words)
+
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+    
+sum_graph(df_speech)
